c29baecbbca468629c658946acb1bae817f6f8af
MADANI <101865800+apolocine@users.noreply.github.com>
2024-05-15 13:23:52 +0200

    Update execition.sh

--- test/execition.sh
+++ test/execition.sh
@@ -1,1 +1,12 @@
+hmd@ubuntu:~/dev$ sudo ./mka2ensite.sh amia.fr watch
+Les fichiers de configuration ont été créés et modifiés pour le site watch.amia.fr avec le store watch
+Enabling site watch.amia.fr.
+To activate the new configuration, you need to run:
+  systemctl reload apache2
+Enabling site watch.amia.fr-ssl.
+To activate the new configuration, you need to run:
+  systemctl reload apache2
+Activation du site
+Redemarrage du serveur
+hmd@ubuntu:~/dev$
 
